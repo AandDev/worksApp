b87dde304db7c2ab928527e509168c2d1d68dc58
Andres <andha037@gmail.com>
2020-09-10 20:35:13 -0300

    Fixing things

--- js/app.js
+++ js/app.js
@@ -1,44 +1,69 @@
-const worksBtn = document.querySelectorAll('.work-btn');
-const vocalesCont = document.querySelector('.vocales');
+const wordsBtn = document.querySelectorAll('.word-btn');
+const vocalesWrap = document.querySelector('.vocales');
+const sounds = document.querySelectorAll('.sound');
 
-const sentences = [
+const vocalesSample = [
     ['Avión', 'Aire', 'Agua'],
-    ['Empanada', 'Ensalada', 'Estación'],
+    ['Empanada', 'Estrella', 'Elefante'],
     ['Iglesia', 'Internet', 'Imagen'],
     ['Opera', 'Oceano', 'Oreja'],
-    ['Unión', 'Uva', 'Umami']
+    ['Uno', 'Uva', 'Uña']
 ];
 const colors = ['#FFD600', '#2bdd52', '#2EADF4', '#CD2EF4', '#F42E2E'];
 
 
-worksBtn.forEach((workBtn, index) => {
-    workBtn.addEventListener('click', () => {
+wordsBtn.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
         const examplesBox = document.createElement('div');
         examplesBox.classList.add('examples');
         examplesBox.innerHTML = 
         `
         <div class="exp-item" style="background-color: ${colors[index]}">
             <button class="exp-btn">
-                ${sentences[index][0]}
+                ${vocalesSample[index][0]}
             </button>
+            <audio class="sound-2" src="../sounds/owl.mp3"></audio>
         </div>
         <div class="exp-item" style="background-color: ${colors[index]}">
             <button class="exp-btn">
-                ${sentences[index][1]}
+                ${vocalesSample[index][1]}
             </button>
+            <audio class="sound-2" src="../sounds/pig.mp3"></audio>
         </div>
         <div class="exp-item" style="background-color: ${colors[index]}">
             <button class="exp-btn">
-                ${sentences[index][2]}
+                ${vocalesSample[index][2]}
             </button>
+            <audio class="sound-2" src="../sounds/elephant.mp3"></audio>
         </div>
         `;
-        //vocalesCont.appendChild(examplesBox);
-        if(vocalesCont.hasChildNodes){
-            vocalesCont.removeChild(vocalesCont.lastChild);
-            vocalesCont.appendChild(examplesBox);
+
+        // sounds[index].currentTime = 0;
+        // sounds[index].play();
+
+        if(vocalesWrap.hasChildNodes){
+            vocalesWrap.removeChild(vocalesWrap.lastChild);
+            vocalesWrap.appendChild(examplesBox);
         } else {
-            vocalesCont.appendChild(examplesBox);
+            vocalesWrap.appendChild(examplesBox);
         }
+    })
+});
+
+
+// ---------- Sonidos de palabras ---------- //
+
+const expBtn = document.querySelectorAll('.exp-btn');
+const sounds2 = document.querySelectorAll('.sound-2');
+
+expBtn.forEach((btn, index) => {
+
+    btn.addEventListener('click', (e) => {
+
+        console.log(e.target);
+        
+        sounds2[index].currentTime = 0;
+        sounds2[index].play();
+
     })
 })
